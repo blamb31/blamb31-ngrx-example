a8ca4fd3648e6c07d7ae5fae98a9311a49430238
Blake Lamb <blakesmac@blakes-mbp.lan>
2021-05-25 01:07:10 -0600

    Added in the end files

---
+++ end/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CheckboxComponent } from './checkbox/checkbox.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CheckboxComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
